bae14c5ac1b93b14d5977d9af05072774e26c718
angelacantfly <owlroro@WL-209-204.WPA.Claremont.Edu>
2015-04-10 23:53:17 -0700

    felis is a piece of shit and cant make buttons.

--- game/game/GameScene.m
+++ game/game/GameScene.m
@@ -19,8 +19,10 @@
 //    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
 //                                   CGRectGetMidY(self.frame));
     
-    SKSpriteNode *p1Feed = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-                                  
+    CGSize size = CGSizeMake(30, 30);
+    SKSpriteNode *p1Feed = [SKSpriteNode spriteNodeWithColor:[UIColor whiteColor] size:size];
+//    SKSpriteNode *p1Feed = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+    
     p1Feed.position = CGPointMake(self.size.width/7, self.size.height/5);
     
 
@@ -40,25 +42,28 @@
 
     
     [self addChild:p1Feed];
+    [self addChild:p1Swap];
+    [self addChild:p2Feed];
+    [self addChild:p2Swap];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
     for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+//        CGPoint location = [touch locationInNode:self];
+//        
+//        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+//        
+//        sprite.xScale = 0.5;
+//        sprite.yScale = 0.5;
+//        sprite.position = location;
+//        
+//        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+//        
+//        [sprite runAction:[SKAction repeatActionForever:action]];
+//        
+//        [self addChild:sprite];
     }
 }
 
