ede60364676d7e96e0e0a199739c7cbc253dde87
angelacantfly <owlroro@WL-209-204.WPA.Claremont.Edu>
2015-04-10 21:05:16 -0700

    initial project

---
+++ game/game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  game
+//
+//  Created by ANGELA ZHOU on 4/10/15.
+//  Copyright (c) 2015 ANGELA ZHOU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ game/game/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  game
+//
+
+//  Copyright (c) 2015 ANGELA ZHOU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
